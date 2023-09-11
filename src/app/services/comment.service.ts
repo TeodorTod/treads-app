16ad7adb46b25193f17fdc9c3fa0bab5f06b23ca
Teodor Todorov <teodor.at.tod@gmail.com>
2023-09-11 22:22:33 +0300

    commit

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,20 +1,35 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable, inject } from "@angular/core";
-import { environment } from "../environment";
-import { Comment } from "../interfaces/comment.interface";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Comment } from '../interfaces/comment.interface';
+import { environment } from '../environment';
+
+
+type CreateCommentDto = {
+  parentId?: string;
+  text: string;
+  userId: string;
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
-  http = inject(HttpClient)
+  http = inject(HttpClient);
 
   getComments(parentId: string = '') {
     let url = `${environment.apiBaseUrl}/comments`;
     if (parentId) {
-      url += `?parentId${parentId}`
+      url += `?parentId=${parentId}`;
     }
-    return this.http.get<Comment[]>(url)
+    console.log(parentId);
+    
+    return this.http.get<Comment[]>(url);
   }
- 
+
+  createComment(comment: CreateCommentDto) {
+    return this.http.post<Comment>(
+      `${environment.apiBaseUrl}/comments`,
+      comment
+    );
+  }
 }
