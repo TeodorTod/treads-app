731d3c1826dc32893512f1725439c51305361ec8
Teodor Todorov <teodor.at.tod@gmail.com>
2023-09-14 22:06:24 +0300

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { CommentComponent } from '../components/comment/comment.component';
 import { CommentService } from '../services/comment.service';
 import { Comment } from '../interfaces/comment.interface';
+import { CommentFormComponent } from '../components/comment-form/comment-form.component';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CommentComponent],
+  imports: [CommonModule, CommentComponent, CommentFormComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   commentService = inject(CommentService);
   comments = signal<Comment[]>([])
+  userService = inject(UserService);
 
   ngOnInit(): void {
     this.getComments();
@@ -24,6 +27,20 @@ export class HomeComponent implements OnInit {
       .subscribe((comments) => {
         this.comments.set(comments)
       })
+  }
+
+  createComment(formValues: {text: string}) {
+    const {text} = formValues;
+    const user = this.userService.getUserFromStorage();
+    if (!user) {
+      return;
+    }
+    this.commentService.createComment({
+      text,
+      userId: user._id
+    }).subscribe(createdComment => {
+      this.comments.set([createdComment, ...this.comments()])
+    })
   }
 
 }
